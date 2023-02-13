1a8083730163e6bc3a9430045896b2d51d910f3a
codeelevate321 <500098457@stu.upes.ac.in>
2023-02-13 22:18:43 +0530

    added

---
+++ src/main/java/mypkg/Myclass.java
@@ -0,0 +1,17 @@
+package mypkg;
+
+public class Myclass {
+	public void show() {
+		System.out.println("welcoming messege for the jenkins user");
+		System.out.println("wmaking changes");
+
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Myclass ob=new Myclass();
+ob.show();
+	}
+
+}
